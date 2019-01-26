06d8123d8bc3ced7beff81c14690f94d5ff8acce
Hamza Aljazara <haljazara@winfooz.com>
2019-01-27 00:07:45 +0200

    fix

--- src/main/java/com/nurseryapi/NurseryApiApplication.java
+++ src/main/java/com/nurseryapi/NurseryApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:dev/application.properties")
+@PropertySource("classpath:${spring.profiles.active:dev}/application.properties")
 public class NurseryApiApplication extends SpringBootServletInitializer {
 
 	/**
